d2877d17e088d55c0783fff07da904efac34b65f
asume21 <asume21@github.com>
2025-09-17 20:14:22 -0600

    fix: update Vite config to explicitly include Babel React JSX plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,15 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }]
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -13,4 +21,7 @@ export default defineConfig({
     port: 5173,
     host: true,
   },
+  optimizeDeps: {
+    include: ['@babel/plugin-transform-react-jsx']
+  }
 });
