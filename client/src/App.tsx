c89d59e79157c128dca4892a730241457aed9624
asume21 <asume21@github.com>
2025-09-26 17:25:15 -0600

    test: create static HTML test to verify server is working - no React or complex dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
-function App() {
+// This file is not being used for the static HTML test
+// Keeping it here in case we need to switch back to React
+
+export default function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
       <h1 style={{ color: '#333' }}>Hello World!</h1>
@@ -6,5 +9,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
