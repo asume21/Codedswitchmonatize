ff33aa556d751619b055deaee085af8a5b0e18a7
asume21 <asume21@github.com>
2025-10-20 17:20:54 -0600

    FIX-remove-unused-db-import-typescript-error

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
-import { db } from "../db";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { MemStorage, DatabaseStorage, type IStorage } from "./storage";
